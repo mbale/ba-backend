918802180d55b70f4c5c21136922eab98a8d3681
Balazs Mocsai <me@bael.me>
2017-03-30 19:29:42 +0200

    documentation & test run
    Signed-off-by: Balazs Mocsai <me@bael.me>

--- test/test.js
+++ test/test.js
@@ -1,10 +1,13 @@
 const Code = require('code');
 const Lab = require('lab');
-const lab = exports.lab = Lab.script();
+
+const lab = Lab.script();
 
 lab.experiment('math', () => {
-    lab.test('returns true when 1 + 1 equals 2', (done) => {
-        Code.expect(1 + 1).to.equal(2);
-        done();
-    });
+  lab.test('returns true when 1 + 1 equals 2', (done) => {
+    Code.expect(1 + 1).to.equal(2);
+    done();
+  });
 });
+
+exports.lab = lab;
