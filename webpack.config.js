15d375aea86418e508b2faf60ada228eb0b7163b
sebmor <sebastian.mortelmans@gmail.com>
2017-03-30 20:02:50 +0200

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,46 @@ const path = require('path');
 const fs = require('fs');
 
 const nodeModules = {};
-
-// requre all external npm packages as commonjs
+// require all external npm packages as commonjs
 fs
-    .readdirSync('node_modules')
-    .filter((x) => ['.bin'].indexOf(x) === -1)
-    .forEach((mod) => {
-        nodeModules[mod] = `commonjs ${mod}`;
-    });
+  .readdirSync('node_modules')
+  .filter(x => ['.bin'].indexOf(x) === -1)
+  .forEach((mod) => {
+    nodeModules[mod] = `commonjs ${mod}`;
+  });
 
 module.exports = {
-    entry: './src/index.js',
-    target: 'node',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'app.js',
-    },
-    plugins: [
-        // setting env variables
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('development'),
-            },
-        }),
-        new webpack.BannerPlugin(
-            'require("source-map-support").install();',
-            {
-                raw: true, entryOnly: false,
-            }
-        ),
+  entry: './src/index.js',
+  target: 'node',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+  },
+  plugins: [
+    // setting env variables
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new webpack.BannerPlugin({
+      banner: 'require("source-map-support").install();',
+      raw: true,
+      entryOnly: false,
+    }),
+  ],
+  module: {
+    rules: [
+      // babel loader
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
     ],
-    module: {
-        loaders: [
-            // babel loader
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-            },
-        ],
-    },
-    // external npm packages for requiring
-    externals: nodeModules,
-    devtool: 'sourcemap',
-    node: {
-        __dirname: false,
-    },
+  },
+  // external npm packages for requiring
+  externals: nodeModules,
+  devtool: 'sourcemap',
+  node: {
+    __dirname: false,
+  },
 };
