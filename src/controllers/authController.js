3f1a77775c96f3512447f8db8c8ac4115b9de4c7
Balazs Mocsai <me@bael.me>
2017-04-10 09:25:07 +0200

    partially implemented auth basic
    Signed-off-by: Balazs Mocsai <me@bael.me>

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,18 @@
-import mongorito from 'mongorito';
-// import userModel from '~/models/userModel.js';
+import UserModel from '~/models/userModel.js';
+import AccessTokenModel from '~/models/accessTokenModel.js';
 
 export default {
   basic(request, reply) {
     const {
       username,
       password,
-    } = request.payload;   
+    } = request.payload;
+
+    return UserModel
+      .find({
+        username,
+      })
+      .then(UserModel.hashPassword('s'))
+      .catch(error => reply.badImplementation(error));
   },
 };
