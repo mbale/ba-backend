c259133f7abe0b33254f0452cec49aa7fedff14f
Balazs Mocsai <me@bael.me>
2017-04-10 15:15:36 +0200

    jwt config
    Signed-off-by: Balazs Mocsai <me@bael.me>

--- src/config.js
+++ src/config.js
@@ -6,6 +6,12 @@ const Config = {
     db: {
       mongoURI: 'mongodb://esportsinsight:dev@146.185.169.74:27017/esportsinsight',
     },
+    jwt: {
+      key: 'dev',
+      options: {
+        expiresIn: '14 days',
+      },
+    },
   },
   mixed: {
     ex: '',
