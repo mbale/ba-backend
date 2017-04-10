1b034622a16540c30f576bfc7cd763b694d7288a
Balazs Mocsai <me@bael.me>
2017-04-10 09:25:40 +0200

    route logic structuring
    Signed-off-by: Balazs Mocsai <me@bael.me>

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,7 @@ import joi from 'joi';
 
 const AuthRoutes = [
   {
-    path: '/v1/auth',
+    path: '/v1/auth/basic',
     method: 'POST',
     handler: AuthController.basic,
     config: {
@@ -15,6 +15,19 @@ const AuthRoutes = [
       },
     },
   },
+  // {
+  //   path: '/v1/auth',
+  //   method: 'POST',
+  //   handler: AuthController.steam,
+  //   config: {
+  //     validate: {
+  //       payload: joi.object().keys({
+  //         steamId: 
+  //         passw
+  //       });
+  //     },
+  //   },
+  // },
 ];
 
 export default AuthRoutes;
