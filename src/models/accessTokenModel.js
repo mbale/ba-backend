693badfbeb6e729aacdfe073ea6ceaf438909e5f
Balazs Mocsai <me@bael.me>
2017-04-10 09:24:47 +0200

    basic tokenmodel
    Signed-off-by: Balazs Mocsai <me@bael.me>

---
+++ src/models/accessTokenModel.js
@@ -0,0 +1,25 @@
+import {
+  Model,
+} from 'mongorito';
+import Joi from 'joi';
+
+class AccessToken extends Model {
+  collection() {
+    return 'accessTokens';
+  }
+
+  configure() {
+    // sch
+    this.schema = Joi.object().keys({
+
+    });
+
+    this.before('create', 'schemaValidation');
+  }
+
+  schemaValidation(next) {
+    
+  }
+}
+
+export default AccessToken;
