077f168429241ef7ae5e65d5951eb0e05a67c8a7
Balazs Mocsai <me@bael.me>
2017-04-16 18:08:38 +0200

    removing social provider from def schema
    Signed-off-by: Balazs Mocsai <me@bael.me>

--- src/models/migration/userMigration.js
+++ src/models/migration/userMigration.js
@@ -17,7 +17,6 @@ const UserMigration = {
     server.log(['database'], `Initiating user's schema with version: ${this.get('_version')}`);
     this.set('accessToken', '');
     this.set('recoveryHash', '');
-    this.set('socialProviders', []);
   },
   1() {
     server.log(['database'], `Upgrading user's schema version: ${this.get('_version')} to 1.`);
