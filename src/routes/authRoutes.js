e70091ce5f2cefb68e147dcdd35ab5c598259abe
Balazs Mocsai <me@bael.me>
2017-05-01 15:53:52 +0200

    added auth validation messages
    Signed-off-by: Balazs Mocsai <me@bael.me>

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import AuthController from '~/controllers/authController.js';
 import joi from 'joi';
+import _ from 'lodash';
 
 const AuthRoutes = [
   {
@@ -23,6 +24,23 @@ const AuthRoutes = [
           username: joi.string().required(),
           password: joi.string().required(),
         }),
+        failAction(request, reply, source, error) {
+          const validationObj = {};
+          validationObj.data = error.data.details[0].path;
+          delete error.output.payload.message;
+          delete error.output.payload.validation;
+          delete error.output.headers;
+
+          switch (error.data.details[0].type) {
+          case 'any.required':
+            validationObj.code = 1;
+            break;
+          default:
+            validationObj.code = 0;
+          }
+          error.output.payload.validationError = validationObj;
+          reply(error.output).code(error.output.statusCode);
+        },
       },
     },
   },
@@ -41,8 +59,34 @@ const AuthRoutes = [
           steamId: joi.string().length(17).required(),
           username: joi.string().optional().allow(''),
           email: joi.string().email().optional(),
-          password: joi.string().optional().allow(''),
+          password: joi.string().optional().min(6),
         }),
+        failAction(request, reply, source, error) {
+          const validationObj = {};
+          validationObj.data = error.data.details[0].path;
+          delete error.output.payload.message;
+          delete error.output.payload.validation;
+          delete error.output.headers;
+
+          switch (error.data.details[0].type) {
+          case 'any.required':
+            validationObj.code = 1;
+            break;
+          case 'string.length':
+            validationObj.code = 2;
+            break;
+          case 'string.email':
+            validationObj.code = 3;
+            break;
+          case 'string.min':
+            validationObj.code = 4;
+            break;
+          default:
+            validationObj.code = 0;
+          }
+          error.output.payload.validationError = validationObj;
+          reply(error.output).code(error.output.statusCode);
+        },
       },
     },
   },
