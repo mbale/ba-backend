c91df706328fe072d1f808e1410aa4d6308cda56
Balazs Mocsai <me@bael.me>
2017-05-01 15:56:12 +0200

    added validation messages for users
    Signed-off-by: Balazs Mocsai <me@bael.me>

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,5 +1,6 @@
 import UsersController from '~/controllers/usersController.js';
 import joi from 'joi';
+import _ from 'lodash';
 
 const UsersRoutes = [
   {
@@ -11,9 +12,32 @@ const UsersRoutes = [
       validate: {
         payload: joi.object().keys({
           username: joi.string().required(),
-          password: joi.string().required(),
+          password: joi.string().required().min(6),
           email: joi.string().email().optional().allow(''),
         }),
+        failAction(request, reply, source, error) {
+          const validationObj = {};
+          validationObj.data = error.data.details[0].path;
+          delete error.output.payload.message;
+          delete error.output.payload.validation;
+          delete error.output.headers;
+
+          switch (error.data.details[0].type) {
+          case 'any.required':
+            validationObj.code = 1;
+            break;
+          case 'number.min':
+            validationObj.code = 2;
+            break;
+          case 'string.email':
+            validationObj.code = 3;
+            break;
+          default:
+            validationObj.code = 0;
+          }
+          error.output.payload.validationError = validationObj;
+          reply(error.output).code(error.output.statusCode);
+        },
       },
     },
   },
