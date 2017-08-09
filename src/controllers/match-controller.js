adbdb496b2b38f2ad68f2b91f80e90683f9b2857
Balazs Mocsai <me@bael.me>
2017-08-09 11:13:22 +0200

    [fix] packback did not contain builtin object extend

--- src/controllers/match-controller.js
+++ src/controllers/match-controller.js
@@ -3,6 +3,8 @@ import {
 } from 'mongorito';
 import Match from '../models/match-model.js';
 import League from '../models/league-model.js';
+import Team from '../models/team-model.js';
+import Game from '../models/game-model.js';
 
 class MatchController {
   static async getMatches(request, reply) {
@@ -15,11 +17,43 @@ class MatchController {
 
       const matchesAsObject = [];
 
-      for (const match of matches) {
-        matchesAsObject.push(match.get());
-      }
+      for (const match of matches) { // eslint-disable-line
+        console.log(await match.get('_id'))
+        let {
+          homeTeamId,
+          awayTeamId,
+          leagueId,
+          gameId,
+        } = await match.get(); // eslint-disable-line
 
-      await Promise.all(matchesAsObject);
+        homeTeamId = new ObjectId(homeTeamId);
+        awayTeamId = new ObjectId(awayTeamId);
+        leagueId = new ObjectId(leagueId);
+        gameId = new ObjectId(gameId);
+
+        const league = await League.findOne({
+          _id: leagueId,
+        });
+
+        const homeTeam = await Team.findOne({
+          _id: homeTeamId,
+        });
+
+        const awayTeam = await Team.findOne({
+          _id: awayTeamId,
+        });
+
+        const game = await Game.findOne({
+          _id: gameId,
+        });
+
+        matchesAsObject.push({
+          league: await league.get('name'),
+          homeTeam: await homeTeam.get('name'),
+          awayTeam: await awayTeam.get('name'),
+          game: await game.get('name'),
+        });
+      }
 
       return reply(matchesAsObject);
     } catch (error) {
