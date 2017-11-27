188203a47dc8961384a43eac644e9a196993c0b4
Balazs Mocsai <me@bael.me>
2017-11-27 12:54:40 +0100

    now get matches does correctly map to services

--- src/controllers/match-controller.ts
+++ src/controllers/match-controller.ts
@@ -1,10 +1,19 @@
-import { Request, Response, ReplyNoContinue } from 'hapi';
-import { badImplementation } from 'boom';
-import { getConnection } from 'typeorm';
-import { ObjectId } from 'bson';
-import { Match } from 'ba-common';
 import MatchService from '../service/match';
 import TeamService from '../service/team';
+import { badImplementation } from 'boom';
+import {
+  Game,
+  League,
+  Match,
+  MatchMapType,
+  MatchOdds,
+  MatchStatusType,
+  MatchUpdate,
+  Team,
+} from 'ba-common';
+import { getConnection, ObjectID } from 'typeorm';
+import { ObjectId } from 'bson';
+import { ReplyNoContinue, Request, Response } from 'hapi';
 
 class MatchController {
   /**
@@ -26,18 +35,17 @@ class MatchController {
         limit : number;
       } = request.query;
 
-      console.log(page)
-      console.log(limit)
-
       const matches = await MatchService.getMatches(null, limit, page);
+
+      type Buffer = [
+        Promise<Team[]>|Promise<Game[]>|ObjectID|Date|MatchOdds[]|MatchUpdate[]
+      ];
 
       let mainBuffer : any[] = [];
 
       for (const match of matches) {
-        const buffer = [];
-
-        // order is important
-        buffer.push(
+        const buffer = [
+          // order
           TeamService.getTeams([match.homeTeamId, match.awayTeamId]),
           TeamService.getGames([match.gameId]),
           MatchService.getLeagues([match.leagueId]),
@@ -45,14 +53,85 @@ class MatchController {
           match.date,
           match.odds,
           match.updates,
-        );
+        ];
 
         mainBuffer.push(buffer);
       }
 
-      mainBuffer = await Promise.all(mainBuffer.map((match => Promise.all(match))));
+      mainBuffer = await Promise.all(mainBuffer.map((buffer => Promise.all(buffer))));
 
-      return reply(mainBuffer);
+      const matchesResponse : MatchResponse[] = [];
+
+      for (const buffer of mainBuffer) {
+        // order
+        const [
+          teams,
+          games,
+          leagues,
+          id,
+          date,
+          odds,
+          updates,
+        ] : [
+          Team[],
+          Game[],
+          League[],
+          ObjectID,
+          Date,
+          MatchOdds[],
+          MatchUpdate[]
+        ] = buffer; //
+
+        const state = {
+          scores : {
+            homeTeam: null,
+            awayTeam: null,
+          },
+          type : null,
+        };
+
+        if (updates.length !== 0) {
+          const orderedUpdates = updates
+            .sort((a, b) => new Date(b.endDate).getTime() - new Date(a.endDate).getTime());
+
+          state.type = orderedUpdates[0].statusType;
+
+          if (orderedUpdates[0].statusType === MatchStatusType.Settled) {
+            state.scores.homeTeam = orderedUpdates[0].homeTeamScore;
+            state.scores.awayTeam = orderedUpdates[1].awayTeamScore;
+          }
+        }
+
+        matchesResponse.push({
+          id,
+          homeTeam: teams[0].name,
+          awayTeam: teams[1].name,
+          league: leagues[0].name,
+          game: games[0].name,
+          gameSlug: games[0].slug,
+          isLive: new Date(date).getTime() === new Date().getTime(),
+          state,
+        });
+      }
+
+      interface MatchResponse {
+        id : ObjectID;
+        homeTeam : string;
+        awayTeam : string;
+        league : string;
+        game : string;
+        gameSlug : string;
+        isLive : boolean;
+        state: {
+          scores : {
+            homeTeam : number;
+            awayTeam : number;
+          },
+          type : MatchStatusType,
+        };
+      }
+
+      return reply(matchesResponse);
     } catch (error) {
       return reply(badImplementation(error));
     }
